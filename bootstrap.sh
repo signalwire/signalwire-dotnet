dfe08bad13a6a94eecca0918b9d77b36af8d5484
Shane Bryldt <astaelan@gmail.com>
2018-09-19 07:49:06 -0600

    Fixed bootstrap.sh to sanitize the laml.patch in case CRLF line endings are used (as with windows) which caused applying the patch to fail

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,6 @@
 rm -rf laml
 git clone https://github.com/twilio/twilio-csharp.git laml
-git apply --directory laml laml.patch
+sed 's/^M$//' laml.patch > laml.clean.patch
+git apply --directory laml laml.clean.patch
+rm -f laml.clean.patch
 grep -Rl "\.twilio\.com" laml/* | xargs sed -i 's/.twilio.com/.signalwire.com/g'
