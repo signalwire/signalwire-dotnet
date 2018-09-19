0b571aeae4317123230a1be3fe73bfb5f28d32f3
Shane Bryldt <astaelan@gmail.com>
2018-09-19 10:54:36 -0600

    Added required injection of the "domain" which will only replace "api" when it's used, other domains are left as is.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,10 @@
+echo Bootstrapping...
 rm -rf laml
 git clone https://github.com/twilio/twilio-csharp.git laml
+echo Patching...
 sed 's/^M$//' laml.patch > laml.clean.patch
 git apply --directory laml laml.clean.patch
 rm -f laml.clean.patch
-grep -Rl "\.twilio\.com" laml/* | xargs sed -i 's/.twilio.com/.signalwire.com/g'
+grep -Rl "\.twilio\.com" laml/src/* | xargs sed -i 's/.twilio.com/.signalwire.com/g'
+grep -Rl "\.twilio\.com" laml/test/* | xargs sed -i 's/.twilio.com/.signalwire.com/g'
+echo Done!
