90aea8d809873423fd4d09192c9cba6bf39e447f
Shane Bryldt <astaelan@gmail.com>
2018-09-20 04:18:27 -0600

    Cleanup on ENV fixture for tests

--- signalwire-dotnet-tests/LaunchSettingsFixture.cs
+++ signalwire-dotnet-tests/LaunchSettingsFixture.cs
@@ -12,23 +12,16 @@ namespace SignalWire
     {
         public LaunchSettingsFixture()
         {
-            using (var file = File.OpenText("Properties\\launchSettings.json"))
+            using (StreamReader sr = File.OpenText(Path.Combine("Properties", "launchSettings.json")))
             {
-                var reader = new JsonTextReader(file);
-                var jObject = JObject.Load(reader);
-
-                var variables = jObject
+                JObject.Load(new JsonTextReader(sr))
                     .GetValue("profiles")
                     .SelectMany(profiles => profiles.Children())
                     .SelectMany(profile => profile.Children<JProperty>())
                     .Where(prop => prop.Name == "environmentVariables")
                     .SelectMany(prop => prop.Value.Children<JProperty>())
-                    .ToList();
-
-                foreach (var variable in variables)
-                {
-                    Environment.SetEnvironmentVariable(variable.Name, variable.Value.ToString());
-                }
+                    .ToList()
+                    .ForEach(p => Environment.SetEnvironmentVariable(p.Name, p.Value.ToString()));
             }
         }
 
