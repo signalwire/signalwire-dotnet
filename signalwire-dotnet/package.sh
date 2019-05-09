fba8be5fff7727aa2e86f8fc8a57179a34374381
Shane Bryldt <shane@signalwire.com>
2019-05-08 20:35:17 -0600

    Fix for bad thrown exception when signalwire space is configured but old deprecated domain isn't (#18)

--- signalwire-dotnet/package.sh
+++ signalwire-dotnet/package.sh
@@ -1,3 +1,4 @@
+export DOTNET_SYSTEM_NET_HTTP_USESOCKETSHTTPHANDLER=0
 rm -rf *.nupkg
 dotnet pack -c Release -o .
 dotnet nuget push `ls *.nupkg` -k $NUGET_APIKEY -s https://api.nuget.org/v3/index.json
