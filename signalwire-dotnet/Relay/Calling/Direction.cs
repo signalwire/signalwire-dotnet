8b288c99d913241e184d6f7093c260d634f0bdae
Joel <joel@signalwire.com>
2019-07-26 15:18:03 -0500

    Implementing fax send and receive

---
+++ signalwire-dotnet/Relay/Calling/Direction.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SignalWire.Relay.Calling
+{
+    public enum Direction
+    {
+        send,
+        receive,
+    }
+}
