713684a898c70bcb7d8dbf96b39fc6832ec384b6
Shane Bryldt <shane@signalwire.com>
2019-08-18 12:39:34 -0600

    Adjusted to change ping timestamp to be double based on epoch

--- signalwire-dotnet/Relay/Blade/Messages/PingParams.cs
+++ signalwire-dotnet/Relay/Blade/Messages/PingParams.cs
@@ -8,7 +8,7 @@ namespace Blade.Messages
     public sealed class PingParams
     {
         [JsonProperty("timestamp", NullValueHandling = NullValueHandling.Ignore)]
-        public string Timestamp { get; set; }
+        public double? Timestamp { get; set; }
 
         [JsonProperty("payload", NullValueHandling = NullValueHandling.Ignore)]
         public string Payload { get; set; }
--- signalwire-dotnet/Relay/Blade/Messages/PingResult.cs
+++ signalwire-dotnet/Relay/Blade/Messages/PingResult.cs
@@ -9,7 +9,7 @@ namespace Blade.Messages
     public sealed class PingResult
     {
         [JsonProperty("timestamp", NullValueHandling = NullValueHandling.Ignore)]
-        public string Timestamp { get; set; }
+        public double? Timestamp { get; set; }
 
         [JsonProperty("payload", NullValueHandling = NullValueHandling.Ignore)]
         public string Payload { get; set; }
