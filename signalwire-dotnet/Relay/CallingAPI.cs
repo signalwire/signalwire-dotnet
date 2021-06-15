76693d90086551e2ecd0fe9996914a246c582df4
Kevin Garabedian <55750465+kevingarabedian2@users.noreply.github.com>
2021-06-14 22:03:36 -0400

    Update CallingAPI.cs

--- signalwire-dotnet/Relay/CallingAPI.cs
+++ signalwire-dotnet/Relay/CallingAPI.cs
@@ -244,7 +244,7 @@ namespace SignalWire.Relay
                         break;
                     }
 
-                // @TODO: sip and webrtc
+                // @TODO: webrtc
                 default:
                     Log(LogLevel.Warning, string.Format("Unknown device type: {0}", receiveParams.Device.Type));
                     return;
